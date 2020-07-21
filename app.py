2f319237fb4b40a98d943f1d38f17f317124cebf
songLilyGit <3383191557@qq.com>
2020-07-21 15:34:00 +0800

    Add index page

--- app.py
+++ app.py
@@ -2,11 +2,11 @@
 2  # -*- coding:utf-8 -*- 
 3  #宋丽丽
 
-from flask import Flask
+from flask import Flask,render_template
 from flask import url_for
 app=Flask(__name__)
 
-@app.route('/')
+
 @app.route('/home')
 @app.route('/index')
 def hello():
@@ -23,4 +23,20 @@ def test_url_for():
     print(url_for('test_url_for'))
     print(url_for('test_url_for',num=2))
     return  'test page'
+
+name = 'songlili'
+movies = [ {'title': 'My Neighbor Totoro', 'year': '1988'},
+           {'title': 'Dead Poets Society', 'year': '1989'},
+           {'title': 'A Perfect World', 'year': '1993'},
+           {'title': 'Leon', 'year': '1994'},
+           {'title': 'Mahjong', 'year': '1996'},
+           {'title': 'Swallowtail Butterfly', 'year': '1996'},
+           {'title': 'King of Comedy', 'year': '1999'},
+           {'title': 'Devils on the Doorstep', 'year': '1999'},
+           {'title': 'WALL-E', 'year': '2008'},
+           {'title': 'The Pork of Music', 'year': '2012'}, ]
+
+@app.route('/dd')
+def index():
+    return render_template('index.html',name=name,movies=movies)
 
